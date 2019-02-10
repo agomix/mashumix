94c4593a28739797e4351bf2a76f29177a1037c5
agomix <47498693+agomix@users.noreply.github.com>
2019-02-10 13:27:21 -0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,19 @@ $(window).load(function(){
 	$('#content').fadeIn(500);
 });
 $(".track.one").click(function(){
-	$(".cover").attr("src","img/song/1.png");
+	$(".cover").attr("src","../img/cover/1.png");
 	$(".track.one").addClass('on');
 	$(".track.two, .track.three").removeClass('on');
 	$(".download").attr("href", "https://docs.google.com/uc?export=download&id=1G8bj7eBGgkMedmRR7PrKtNlR311tzeON")
 });
 $(".track.two").click(function(){
-	$(".cover").attr("src","img/song/2.png");
+	$(".cover").attr("src","../img/cover/2.png");
 	$(".track.two").addClass('on');
 	$(".track.one, .track.three").removeClass('on');
 	$(".download").attr("href", "https://docs.google.com/uc?export=download&id=1q4zm-Ig4BsX2DQHNE6KznSJI6SGUr-Rh")
 });
 $(".track.three").click(function(){
-	$(".cover").attr("src","img/song/3.png");
+	$(".cover").attr("src","../img/cover/3.png");
 	$(".track.three").addClass('on');
 	$(".track.two, .track.one").removeClass('on');
 	$(".download").attr("href", "https://docs.google.com/uc?export=download&id=1NpUh-tLoLvmHrE1HnDpf-mi6h3Q-8yw7")
