b9d5590e9dc904421099b7ff4d9a91660e2949e3
agomix <47498693+agomix@users.noreply.github.com>
2019-02-17 16:40:35 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,37 @@
 $(window).load(function(){
 	$('#loader').fadeOut(500);
 	$('#content').fadeIn(500);
-	$("meta[name='theme-color']").attr("content", "#ac5877");
-	$("meta[name='msapplication-TileColor']").attr("content", "#ac5877");
 });
-$(".track.one").click(function(){
-	$(".cover").attr("src","img/cover/1.png");
+$(".track.one, .cover.one").click(function(){
+	$(".cover.one").addClass('center');
+	$(".cover.two").addClass('left');
+	$(".cover.three").addClass('right');
+	$(".cover.two, .cover.three").removeClass('center');
+	$(".cover.one, .cover.two").removeClass('right');
+	$(".cover.one, .cover.three").removeClass('left');
 	$(".track.one").addClass('on');
 	$(".track.two, .track.three").removeClass('on');
 	$(".download").attr("href", "https://docs.google.com/uc?export=download&id=1G8bj7eBGgkMedmRR7PrKtNlR311tzeON");
-	$(".cover-download").css("background-color", "#ac5877");
-	$("meta[name='theme-color']").attr("content", "#ac5877");
-	$("meta[name='msapplication-TileColor']").attr("content", "#ac5877");
 });
-$(".track.two").click(function(){
-	$(".cover").attr("src","img/cover/2.png");
+$(".track.two, .cover.two").click(function(){
+	$(".cover.one").addClass('left');
+	$(".cover.two").addClass('center');
+	$(".cover.three").addClass('right');
+	$(".cover.one, .cover.three").removeClass('center');
+	$(".cover.one, .cover.two").removeClass('right');
+	$(".cover.two, .cover.three").removeClass('left');
 	$(".track.two").addClass('on');
 	$(".track.one, .track.three").removeClass('on');
 	$(".download").attr("href", "https://docs.google.com/uc?export=download&id=1q4zm-Ig4BsX2DQHNE6KznSJI6SGUr-Rh");
-	$(".cover-download").css("background-color", "#a89b71");
-	$("meta[name='theme-color']").attr("content", "#a89b71");
-	$("meta[name='msapplication-TileColor']").attr("content", "#a89b71");
 });
-$(".track.three").click(function(){
-	$(".cover").attr("src","img/cover/3.png");
+$(".track.three, .cover.three").click(function(){
+	$(".cover.one").addClass('right');
+	$(".cover.two").addClass('left');
+	$(".cover.three").addClass('center');
+	$(".cover.two, .cover.one").removeClass('center');
+	$(".cover.one, .cover.three").removeClass('left');
+	$(".cover.two, .cover.three").removeClass('right');
 	$(".track.three").addClass('on');
 	$(".track.two, .track.one").removeClass('on');
 	$(".download").attr("href", "https://docs.google.com/uc?export=download&id=1NpUh-tLoLvmHrE1HnDpf-mi6h3Q-8yw7");
-	$(".cover-download").css("background-color", "#405c63");
-	$("meta[name='theme-color']").attr("content", "#405c63");
-	$("meta[name='msapplication-TileColor']").attr("content", "#405c63");
 });
